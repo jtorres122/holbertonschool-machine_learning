9bac103ec90f4aa9526ddf3ce3d6475cc74eabac
“Julian” <jose.torres122@outlook.com>
2022-04-13 19:47:35 -0400

    Be a function that transposes a numpy.ndarray matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Module contains a method that transposes a numpy.ndarray matrix'''
+
+
+def np_transpose(matrix):
+    '''Function transposes a numpy.ndarray matrix'''
+
+    return matrix.transpose()
