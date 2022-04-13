a4b735bf70b3cd471e94384905ad4e520e62b02f
“Julian” <jose.torres122@outlook.com>
2022-04-13 09:11:29 -0400

    Add if statements to verify that matrices can be concatenated

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -11,6 +11,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
         mat3 = mat1 + mat2
 
     if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         mat3 = []
         for i in range(len(mat1)):
             mat3.append(mat1[i] + mat2[i])
