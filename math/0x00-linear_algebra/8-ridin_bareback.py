1393183430097507f3c1096f6214319c99effecb
“Julian” <jose.torres122@outlook.com>
2022-04-13 10:27:31 -0400

    Be a prototype of matrix multiplication

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -9,10 +9,9 @@ def mat_mul(mat1, mat2):
               [0, 0, 0, 0],
               [0, 0, 0, 0]]
 
-    if len(mat1[0]) != len(mat2):
-        return None
-
     for i in range(len(mat1)):
+        if len(mat1[i]) != len(mat2):
+            return None
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
                 result[i][j] += mat1[i][k] * mat2[k][j]
