cc4b8e70aefd3c31511d9133b973445b243a36a1
“Julian” <jose.torres122@outlook.com>
2022-04-13 10:11:06 -0400

    Be a prototype of matrix multiplication

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Module contains a method that performs matrix multiplication'''
+
+
+def mat_mul(mat1, mat2):
+    '''Function performs matrix multiplication'''
+
+    result = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+
+    return result
