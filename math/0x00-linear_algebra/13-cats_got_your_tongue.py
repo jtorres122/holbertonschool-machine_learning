9ac737a38e8dba942d6e93c9fad3f36ee4693cbe
“Julian” <jose.torres122@outlook.com>
2022-04-13 20:13:32 -0400

    Be a function that performs matrix multiplication using numpy

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -7,6 +7,6 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    '''Function'''
+    '''Function concatenates two matrices along a specific axis'''
 
     return np.concatenate((mat1, mat2), axis)
---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Module contains a method that performs matrix multiplication'''
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''Function performs matrix multiplication'''
+
+    return np.matmul(mat1, mat2)
