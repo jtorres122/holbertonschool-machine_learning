c452df92a1f01f9ddcee7d4f38e3c81731e5eda0
“Julian” <jose.torres122@outlook.com>
2022-04-20 16:08:17 -0400

    Be a function that calculates the sum of a series=i^2

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Module contains a function that calculates sigma notation'''
+
+
+def summation_i_squared(n):
+    '''Function calculates sum of series'''
+    '''
+    Note: recursion does not work because
+    task asks for NoneType to be returned
+    '''
+
+    if isinstance(n, int) is False:
+        return None
+
+    return (n * (n + 1) * (2 * n + 1)) / 6
