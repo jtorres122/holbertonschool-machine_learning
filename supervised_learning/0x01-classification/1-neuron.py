42b8ab3ca2af7bb2f821b24679369342e1575704
“Julian” <jose.torres122@outlook.com>
2022-05-14 18:31:58 -0400

    Task 1(switches instance attributes from public to private)

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''Module contains Neuron class'''
+import numpy as np
+
+
+class Neuron:
+    '''class Neuron'''
+
+    def __init__(self, nx):
+        '''constructor'''
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''W getter'''
+        return self.__W
+
+    @property
+    def b(self):
+        '''b getter'''
+        return self.__b
+
+    @property
+    def A(self):
+        '''A getter'''
+        return self.__A
