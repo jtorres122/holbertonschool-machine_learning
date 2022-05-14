6825166621d7bdb7ea8a24ce16e38d78307dcb12
“Julian” <jose.torres122@outlook.com>
2022-05-14 17:38:51 -0400

    pycodestyle compliance for task 0

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -12,6 +12,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.normal(size=(1, nx)) # Create a random matrix of size (1, nx)
+        self.W = np.random.normal(size=(1, nx))  # Create a random matrix
         self.b = 0
         self.A = 0
