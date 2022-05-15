42d46655265d5673be989ad9582c50ed6ee0b5e8
“Julian” <jose.torres122@outlook.com>
2022-05-14 22:48:33 -0400

    Task 8, NeuralNetwork class constructor setup

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Module contains NeuralNetwork class'''
+import numpy as np
+
+
+class NeuralNetwork:
+    '''NeuralNetwork Class'''
+
+    def __init__(self, nx, nodes):
+        '''class instantiator'''
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
