d74107d3cf0cef248911c8fe6e2f9f6c4bc7d934
“Julian” <jose.torres122@outlook.com>
2022-05-15 01:15:23 -0400

    Task 10-11, calculates forward propagation and cost of a neural network

---
+++ supervised_learning/0x01-classification/10-neural_network.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+'''Module contains NeuralNetwork class'''
+import numpy as np
+
+
+class NeuralNetwork:
+    '''NeuralNetwork Class'''
+
+    def __init__(self, nx, nodes):
+        '''class instantiator'''
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        '''W1 getter'''
+        return self.__W1
+
+    @property
+    def b1(self):
+        '''b1 getter'''
+        return self.__b1
+
+    @property
+    def A1(self):
+        '''A1 getter'''
+        return self.__A1
+
+    @property
+    def W2(self):
+        '''W2 getter'''
+        return self.__W2
+
+    @property
+    def b2(self):
+        '''b2 getter'''
+        return self.__b2
+
+    @property
+    def A2(self):
+        '''A2 getter'''
+        return self.__A2
+
+    def forward_prop(self, X):
+        '''
+        Method calculates the forward propagation
+        of the neural network
+        '''
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        
+        return self.__A1, self.__A2
