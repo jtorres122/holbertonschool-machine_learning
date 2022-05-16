75a9e52cb8922e8de6b8b3211f3c593d7f9d5da3
“Julian” <jose.torres122@outlook.com>
2022-05-15 21:58:04 -0400

    Task 2, creates the forward propagation graph for the neural network

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -8,9 +8,6 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     '''Creates a forward propagation graph'''
-    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     for i in range(len(layer_sizes)):
-        layer = tf.layers.Dense(units=layer_sizes[i],
-                                activation=activations[i],
-                                kernel_initializer=init)
-    return layer(x)
+        layer = create_layer(x, layer_sizes[i], activations[i])
+    return layer
