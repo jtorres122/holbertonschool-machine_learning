c52a1faf59b1107a51963a9824a96320c71e1522
“Julian” <jose.torres122@outlook.com>
2022-05-15 21:41:33 -0400

    Task 1, pycodestyle compliance

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -6,5 +6,6 @@ import tensorflow.compat.v1 as tf
 def create_layer(prev, n, activation):
     '''Creates a layer of a neural network'''
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=init)
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=init)
     return layer(prev)
