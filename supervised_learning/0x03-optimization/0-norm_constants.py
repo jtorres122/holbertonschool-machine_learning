b65903dd361655fe084c50dd1584f45a4c52e4b5
“Julian” <jose.torres122@outlook.com>
2022-05-19 14:46:04 -0400

    Task 0-2

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''Module contains the function norm_constants'''
+import numpy as np
 
 
 def normalization_constants(X):
@@ -7,6 +8,6 @@ def normalization_constants(X):
     Function calculates the mean and standard deviation
     of each feature, respectively.
     '''
-    mean = X.mean(axis=0)
-    standard_dev = X.std(axis=0)
+    mean = np.mean(X, axis=0)
+    standard_dev = np.std(X, axis=0)
     return mean, standard_dev
