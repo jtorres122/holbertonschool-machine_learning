14f71ab1d864e026b263d9fd189afbbf71ea9c26
“Julian” <jose.torres122@outlook.com>
2022-05-19 21:36:41 -0400

    Task 6, function creates training operation for the NN

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -8,18 +8,5 @@ def create_momentum_op(loss, alpha, beta1):
     Function creates the training operation for a neural network in tensorflow
     using the gradient descent with momentum optimization algorithm
     '''
-    var = tf.trainable_variables()[0]
-    grad = tf.gradients(loss, var)[0]
-    v = tf.Variable(tf.zeros_like(var), trainable=False)
-    v_op = update_variables_momentum(alpha, beta1, var, grad, v)
-    return v_op
-
-
-def update_variables_momentum(alpha, beta1, var, grad, v):
-    '''
-    Function updates a variable using the gradient descent
-    with momentum optimization algorithm
-    '''
-    v = beta1 * v + (1 - beta1) * grad
-    var -= alpha * v
-    return var, v
+    momentum_op = tf.train.MomentumOptimizer(alpha, beta1)
+    return momentum_op.minimize(loss)
