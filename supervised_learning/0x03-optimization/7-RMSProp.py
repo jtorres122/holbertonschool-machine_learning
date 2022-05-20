f5289a8571133890d628c0c102e182a729a9670f
“Julian” <jose.torres122@outlook.com>
2022-05-19 22:17:11 -0400

    Task 7, using numpy

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''Module contains update_variables_RMSProp function'''
+import numpy as np
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     '''Function updates a variable using the RMSProp optimization algorithm'''
-    new_s = beta2 * s + (1 - beta2) * grad ** 2
-    new_var = var - alpha * grad / (s + epsilon) ** 0.5
+    new_s = beta2 * s + (1 - beta2) * (grad ** 2)
+    new_var = var - alpha * (grad / (epsilon + np.sqrt(new_s)))
     return new_var, new_s
