817852d8f27dbc91c5733f1b45183e35d19215f9
“Julian” <jose.torres122@outlook.com>
2022-05-19 23:18:29 -0400

    Task 13, a function that performs batch normalization

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -5,9 +5,9 @@ import tensorflow.compat.v1 as tf
 
 def batch_norm(Z, beta, gamma, epsilon):
     '''
-    Function normalizes an unactivated output 
+    Function normalizes an unactivated output
     of a neural network using batch normalization
     '''
-    batch_mean, batch_var = tf.nn.moments(Z, [0])
-    return tf.nn.batch_normalization(Z, batch_mean, batch_var,
-                                     beta, gamma, epsilon)
+    batch_mean = np.mean(Z, axis=0)
+    batch_var = np.var(Z, axis=0)
+    return (Z - batch_mean) / np.sqrt(batch_var + epsilon) * gamma + beta
