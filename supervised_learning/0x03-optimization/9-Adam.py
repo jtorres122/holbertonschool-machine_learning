abede0d5f64bc55299a306722288ca24038eb0ac
“Julian” <jose.torres122@outlook.com>
2022-05-19 22:36:04 -0400

    Task 9, updates variable using adam optimization algo

---
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Module contains update_variables_Adam function'''
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    '''Function that updates the variables using the Adam optimization algorithm'''
+    v_t = beta1 * v + (1 - beta1) * grad
+    s_t = beta2 * s + (1 - beta2) * grad * grad
+    v_corr = v_t / (1 - beta1 ** t)
+    s_corr = s_t / (1 - beta2 ** t)
+    var_update = var - alpha * v_corr / (s_corr ** 0.5 + epsilon)
+    return var_update, v_t, s_t
