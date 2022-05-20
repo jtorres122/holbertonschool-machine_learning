bccc0617dd773a7288a41dc71e010ca0fc86dafa
“Julian” <jose.torres122@outlook.com>
2022-05-19 23:35:42 -0400

    Task 14, apparently contrib doesn't exist but it does?

---
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -0,0 +1,10 @@
+#1/usr/bin/env python3
+'''Module contains create_batch_norm_layer function'''
+import tensorflow.compat.v1 as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    '''Function'''
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(n, activation=activation, kernel_initializer=init)
+    return layer(prev)
