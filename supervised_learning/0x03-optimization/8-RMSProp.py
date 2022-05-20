2003104b8301adf874d85a34f619f5763062a9c1
“Julian” <jose.torres122@outlook.com>
2022-05-19 22:30:07 -0400

    Task 8, creates the training operation for a neural network in tensorflow using the RMSProp optimization algorithm

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -5,6 +5,5 @@ import tensorflow.compat.v1 as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     '''Function creates the RMSProp optimization operation'''
-    s = tf.Variable(tf.zeros(loss.shape), trainable=False)
-    opt = tf.train.RMSPropOptimizer(alpha, beta2, epsilon, s)
+    opt = tf.train.RMSPropOptimizer(alpha, beta2, epsilon=epsilon)
     return opt.minimize(loss)
