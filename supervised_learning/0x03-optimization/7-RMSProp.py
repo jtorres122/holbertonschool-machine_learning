34fefc96a4d78d9a96f2487bb290d286b4bbb39e
“Julian” <jose.torres122@outlook.com>
2022-05-19 22:10:15 -0400

    Task 7, function that updates a varibale using the RMSProp optimization algorithm

---
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Module contains update_variables_RMSProp function'''
+import tensorflow.compat.v1 as tf
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    '''Function updates a variable using the RMSProp optimization algorithm'''
+    new_s = beta2 * s + (1 - beta2) * grad ** 2
+    new_var = var - alpha * grad / (tf.sqrt(new_s) + epsilon)
+    return new_var, new_s
