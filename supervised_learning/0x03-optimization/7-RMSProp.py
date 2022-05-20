b4d62dc1a3f5834443e07a226490484bc4299d12
“Julian” <jose.torres122@outlook.com>
2022-05-19 22:15:14 -0400

    Task 7, remmoved unnecessary import

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 '''Module contains update_variables_RMSProp function'''
-import tensorflow.compat.v1 as tf
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     '''Function updates a variable using the RMSProp optimization algorithm'''
     new_s = beta2 * s + (1 - beta2) * grad ** 2
-    new_var = var - alpha * grad / (tf.sqrt(new_s) + epsilon)
+    new_var = var - alpha * grad / (s + epsilon) ** 0.5
     return new_var, new_s
