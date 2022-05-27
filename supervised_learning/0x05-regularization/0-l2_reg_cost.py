a25961bf271c973af8bf13df82d64e54babb39c6
Julian <jose.torres122@outlook.com>
2022-05-26 23:09:57 -0400

    reset

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -4,6 +4,3 @@ import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     '''Calculates the cost of a neural network with L2 regularization'''
-    for i in range(1, L + 1):
-        cost += lambtha / (2 * m) * np.sum(weights['W' + str(i)] ** 2)
-    return cost
--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -4,17 +4,4 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     '''Calculates one pass of gradient descent on the neural network'''
-    m = Y.shape[1]
-    dZ = {}
-    for i in range(L, 0, -1):
-        if i == L:
-            dZ[i] = cache['A' + str(i)] - Y
-        else:
-            dZ[i] = np.matmul(weights['W' + str(i + 1)].T,
-                              dZ[i + 1]) * (cache['A' + str(i)] > 0)
-        dW = np.matmul(dZ[i], cache['A' + str(i - 1)].T) / m
-        db = np.sum(dZ[i], axis=1, keepdims=True) / m
-        dW += lambtha / m * weights['W' + str(i)]
-        weights['W' + str(i)] = weights['W' + str(i)] - alpha * dW
-        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
-    return weights
+
