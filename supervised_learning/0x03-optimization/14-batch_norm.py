4ea992b9e55fbad971001073513aa9b5afb53579
“Julian” <jose.torres122@outlook.com>
2022-05-26 22:38:27 -0400

    Main test files and minor changes

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 '''Module contains create_batch_norm_layer function'''
 import tensorflow.compat.v1 as tf
+import tensorflow.keras as keras
 
 
 def create_batch_norm_layer(prev, n, activation):
     '''Function'''
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(n, activation=activation, kernel_initializer=init)
-    return layer(prev)
+    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = keras.layers.Dense(units=n, kernel_initializer=w)
+    mean, variance = tf.nn.moments(layer(prev), axes=[0])
+    gamma = tf.Variable(tf.ones((1, n)), trainable=True)
+    beta = tf.Variable(tf.zeros((1, n)), trainable=True)
+    r = tf.nn.batch_normalization(layer(prev), mean, variance,
+                                  beta, gamma, 1e-8)
+    return activation(r)
---
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module contains moving_average function'''
+import numpy as np
+
+
+def moving_average(data, beta):
+    '''Calculate weighted moving average of dataset'''
+
+
