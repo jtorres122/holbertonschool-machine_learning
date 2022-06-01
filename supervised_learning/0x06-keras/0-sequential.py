0fe5543a1d6bf88e6e18b6ae9a75451653444559
Julian <jose.torres122@outlook.com>
2022-05-31 20:32:08 -0400

    Task 0, pycodestyle and final dropout removed

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -6,15 +6,16 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     '''Function builds a neural network with the Keras library'''
     model = K.Sequential()
+    regularizer = K.regularizers.l2(lambtha)
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(layers[i],
                                      activation=activations[i],
-                                     kernel_regularizer=K.regularizers.l2(lambtha),
+                                     kernel_regularizer=regularizer,
                                      input_shape=(nx,)))
         else:
             model.add(K.layers.Dropout(1 - keep_prob))
             model.add(K.layers.Dense(layers[i],
                                      activation=activations[i],
-                                     kernel_regularizer=K.regularizers.l2(lambtha)))
+                                     kernel_regularizer=regularizer))
     return model
