91f7eb2f84519a7245e437d28e3f563189e91972
Julian <jose.torres122@outlook.com>
2022-05-31 20:23:31 -0400

    Keras project Task 0

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -7,10 +7,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     '''Function builds a neural network with the Keras library'''
     model = K.Sequential()
     for i in range(len(layers)):
-        model.add(K.layers.Dense(layers[i],
-                                 activation=activations[i],
-                                 kernel_regularizer=K.regularizers.l2(lambtha),
-                                 input_shape=(nx,)))
-                                 
-        model.add(K.layers.Dropout(1 - keep_prob))
+        if i == 0:
+            model.add(K.layers.Dense(layers[i],
+                                     activation=activations[i],
+                                     kernel_regularizer=K.regularizers.l2(lambtha),
+                                     input_shape=(nx,)))
+        else:
+            model.add(K.layers.Dropout(1 - keep_prob))
+            model.add(K.layers.Dense(layers[i],
+                                     activation=activations[i],
+                                     kernel_regularizer=K.regularizers.l2(lambtha)))
     return model
