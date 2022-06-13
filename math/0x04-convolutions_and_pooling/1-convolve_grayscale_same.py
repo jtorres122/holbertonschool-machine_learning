d9bf55e945efab3095e3823d6359747f5e4e2a10
Julian <jose.torres122@outlook.com>
2022-06-13 18:14:15 -0400

    Reorganizing file structure

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module contains convolve_grayscale_same function'''
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    '''Function performs a same convolution on grayscale images'''
+
+    
---
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Module contains convolve_grayscale_padding function'''
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    '''Function performs a valid convolution on grayscale images'''
