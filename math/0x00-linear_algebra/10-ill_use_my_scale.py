a57fda9aa2938b02e74d06a7d3ec3642de0d3f3f
“Julian” <jose.torres122@outlook.com>
2022-07-28 21:56:29 -0400

    I just return the shape?

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 '''Module contains np_shape function'''
 
 
 def np_shape(matrix):
     '''Function calculates the shape of a numpy array'''
-    return(matrix.shape)
+    return matrix.shape
