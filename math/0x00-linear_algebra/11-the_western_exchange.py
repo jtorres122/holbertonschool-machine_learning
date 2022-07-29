43e94b418a2b68d0cb0c4108d95becba5bf53c39
“Julian” <jose.torres122@outlook.com>
2022-07-28 22:18:33 -0400

    Practice using numpy, now onto the logic

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Module contains np_transpose function'''
+
+
+def np_transpose(matrix):
+    '''Function transposes a numpy array'''
+    return matrix.transpose()
---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Module contains np_matmul function'''
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''Function performs matrix multiplication'''
+    return np.matmul(mat1, mat2)
