Gradient Descent and correct return in evaluate method

supervised_learning/0x01-classification/4-neuron.py
@@ -48,4 +48,4 @@ class Neuron:
         '''Function evaluates the neuron's predictions'''
         A = self.forward_prop(X)
         a = np.where(A <= 0.5, 0, 1)
-        return A, self.cost(Y, A)
+        return a, self.cost(Y, A)