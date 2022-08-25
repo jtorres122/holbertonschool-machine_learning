18099863ea208f5bf32330ca5d93f29108893226
“Julian” <jose.torres122@outlook.com>
2022-08-25 17:45:02 -0400

    function creates a layer

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Module contains create_layer function'''
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def create_layer(prev, n, activation):
+    '''Function creates a tensor layer'''
+    tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(name="layer", units=n, activation=activation)
+    y = layer(prev)
+    return y
