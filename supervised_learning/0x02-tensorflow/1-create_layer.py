b4a9b4250fa3f48937fa95048308ee56dc2c301f
“Julian” <jose.torres122@outlook.com>
2022-08-25 17:51:27 -0400

    creates a layer with intitializer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -6,7 +6,9 @@ tf.disable_v2_behavior()
 
 def create_layer(prev, n, activation):
     '''Function creates a tensor layer'''
-    tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.keras.layers.Dense(name="layer", units=n, activation=activation)
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(
+        name="layer", units=n, activation=activation,
+        kernel_initializer=initializer)
     y = layer(prev)
     return y
