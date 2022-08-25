fdc2d888e55bea23cf597035e0c38ea5d50b38e2
“Julian” <jose.torres122@outlook.com>
2022-08-25 16:19:41 -0400

    Disable tensorflow.v2 behavior, to allow for desired output

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''Module contains create_placeholders function'''
 import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def create_placeholders(nx, classes):
     '''Function returns two placeholders for the neural network'''
     x = tf.placeholder(name="x", shape=(None, nx), dtype=tf.float32)
-    y = tf.placeholder(name="y", shape=(None, nx), dtype=tf.float32)
+    y = tf.placeholder(name="y", shape=(None, classes), dtype=tf.float32)
     return x, y
