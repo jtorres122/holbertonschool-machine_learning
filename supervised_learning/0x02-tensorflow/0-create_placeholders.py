80887ecdaf31cd0e0b613f88a041010ee5919bad
Julian <jose.torres122@outlook.com>
2022-08-24 23:29:25 -0400

    pycodestyle indentation fix

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -4,7 +4,7 @@ import tensorflow.compat.v1 as tf
 
 
 def create_placeholders(nx, classes):
-	'''Function returns two placeholders for the neural network'''
-	x = tf.placeholder(name="x", shape=nx, dtype=tf.float32)
-	y = tf.placeholder(name="y", shape=nx, dtype=tf.float32)
-	return x, y
+    '''Function returns two placeholders for the neural network'''
+    x = tf.placeholder(name="x", shape=nx, dtype=tf.float32)
+    y = tf.placeholder(name="y", shape=nx, dtype=tf.float32)
+    return x, y
