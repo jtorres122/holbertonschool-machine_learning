e1a95ee0a4a2fbdce64cbe87d0aca54364c91101
“Julian” <jose.torres122@outlook.com>
2022-08-25 16:15:28 -0400

    Fix the shape of the placeholders

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -5,6 +5,6 @@ import tensorflow.compat.v1 as tf
 
 def create_placeholders(nx, classes):
     '''Function returns two placeholders for the neural network'''
-    x = tf.placeholder(name="x", shape=nx, dtype=tf.float32)
-    y = tf.placeholder(name="y", shape=nx, dtype=tf.float32)
+    x = tf.placeholder(name="x", shape=(None, nx), dtype=tf.float32)
+    y = tf.placeholder(name="y", shape=(None, nx), dtype=tf.float32)
     return x, y
