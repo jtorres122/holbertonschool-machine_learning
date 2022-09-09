a9335983d638753e31e5d74ceaf4adba360e921e
Julian <jose.torres122@outlook.com>
2022-09-08 22:47:58 -0400

    Task 6 pycodestyle compliance

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -9,5 +9,6 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     weight = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
     dropout = tf.layers.Dropout(1 - keep_prob)
     layer = tf.layers.Dense(units=n, activation=activation,
-                        kernel_initializer=weight, kernel_regularizer=dropout)
+                            kernel_initializer=weight,
+                            kernel_regularizer=dropout)
     return layer(prev)
