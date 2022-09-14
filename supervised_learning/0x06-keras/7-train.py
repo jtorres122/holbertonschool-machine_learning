0df489e19c64ad1607d677bcbad92d91c04c9931
Julian <jose.torres122@outlook.com>
2022-09-13 22:13:49 -0400

    Task 7

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -5,10 +5,21 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
-                patience=0, verbose=True, shuffle=False):
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
     '''Function trains a model using mini-batch gradient descent'''
+
     if validation_data:
-        callbacks = K.callbacks.EarlyStopping(patience=patience)
+        def scheduler(epoch):
+            '''
+            gets the learning rate of each epoch
+            :epoch: is the current epoch
+            '''
+            return alpha / (1 + decay_rate * epoch)
+            
+        callbacks = []
+        callbacks.append(K.callbacks.EarlyStopping(patience=patience))
+        callbacks.append(K.callbacks.LearningRateScheduler(scheduler, verbose=1))
         return network.fit(x=data, y=labels, batch_size=batch_size,
                            epochs=epochs, callbacks=[callbacks],
                            validation_data=validation_data,
