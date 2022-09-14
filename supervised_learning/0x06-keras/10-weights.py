72ef7e28a6af31a9583ff72afd9ac0ad99fc0e48
Julian <jose.torres122@outlook.com>
2022-09-13 22:37:19 -0400

    Task 8 and 10

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Module contains load and save weight fucntions'''
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    '''Function saves a model's weight'''
+    network.save_weights(filename, save_format=save_format)
+    return None
+
+
+def load_weights(network, filename):
+    '''Function loads a model's weight'''
+    network.load_weights(filename)
+    return None
