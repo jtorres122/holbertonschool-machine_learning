176af9283a4283984fee7c44b556e8106d04baa2
Julian <jose.torres122@outlook.com>
2022-09-15 22:17:04 -0400

    broke a** checkers

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -8,22 +8,19 @@ def convolve_grayscale_padding(images, kernel, padding):
     Function performs a convolution on grayscale
     images with custom padding
     '''
-    m, h, w = images.shape
-    kh, kw = kernel.shape
-
-    ph = padding[0]
-    pw = padding[1]
-
-    oh = h + 2 * ph - kh + 1
-    ow = w + 2 * pw - kw + 1
-    conv_img = np.zeros((m, oh, ow))
-
-    padded_imgs = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
-                         mode='constant')
-
-    for y in range(images.shape[1]):
-        for x in range(images.shape[2]):
-            img_slice = padded_imgs[:, y:(y + kh), x:(x + kw)]
-            conv_img[:, y, x] = np.tensordot(img_slice, kernel)
-
-    return conv_img
+    m, h, w = images.shape 
+    kh, kw = kernel.shape 
+    ph, pw = padding[0], padding[1] 
+    oh = h + 2 * ph - kh + 1 
+    ow = w + 2 * pw - kw + 1 
+    dim = (m, oh, ow) 
+    out = np.zeros(dim) 
+    padded = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)), 
+                    mode='constant', constant_values=0) 
+    for i in range(dim[1]): 
+        for j in range(dim[2]): 
+            x = i + kh 
+            y = j + kw 
+            M = padded[:, i:x, j:y] 
+            out[:, i, j] = np.tensordot(M, kernel) 
+    return out
