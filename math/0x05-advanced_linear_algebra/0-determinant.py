a1d21ce0b2dcbaecabebb3f4a465cd263c6a6262
Julian <jose.torres122@outlook.com>
2022-11-03 23:07:11 -0400

    Test

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -4,20 +4,20 @@
 
 def determinant(matrix):
     '''Function calculates the determinant of a matrix'''
-    det = 0
-    if matrix == [[]]:
-        return 1
-    if len(matrix) == 1:
-        return matrix[0][0]
     if not all(type(row) == list for row in matrix):
         raise TypeError("matrix must be a list of lists")
     if len(matrix) == 0 or type(matrix) != list:
         raise TypeError("matrix must be a list of lists")
-    if not all(len(row) == len(matrix) for row in matrix):
+    if matrix == [[]]:
+        return 1
+    if not all(len(r) == len(matrix) for r in matrix):
         raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return matrix[0][0]
     if len(matrix) == 2:
         x = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
         return x
+    det = 0
     for x, num in enumerate(matrix):
         temp = []
         P = matrix[0][x]
