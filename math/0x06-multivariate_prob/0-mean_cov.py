22e471b0ac268f8137706a81337dcb1c5bacfa0f
Julian <jose.torres122@outlook.com>
2023-01-13 19:29:30 -0400

    X is not a 2D array

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -6,7 +6,7 @@ import numpy as np
 def mean_cov(X):
     '''Function calculates the mean and covariance of a data set'''
 
-    if X.ndim < 2:
+    if X.shape[0] != 2:
         raise TypeError('X must be a 2D numpy.ndarray')
     if X.shape[0] < 2:
         raise ValueError('X must contain multiple data points')
